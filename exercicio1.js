eea2b4c39d93a09968e8aff47dbfab44fd7ed9af
Lucca Miranda <luccafmsilva@gmail.com>
2021-06-17 22:56:42 -0300

    feat: 👽

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const calc = document.getElementById("calcular");
+
+calc.addEventListener("click", () => {
+    let resultado = document.getElementById("resultado");
+
+    const inputs = Array.from(document.getElementsByTagName("input"));
+
+    const gravitationalForce =
+        (inputs[0].value * inputs[1].value * inputs[2].value) /
+        inputs[3].value ** 2;
+
+    resultado.value = gravitationalForce;
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const expandButtons = document.querySelectorAll(".botao-expandir-retrair");
+
+expandButtons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+        expandButtons.forEach((_, index) => {
+            const parent =
+                e.currentTarget === expandButtons[index] &&
+                expandButtons[index].parentElement;
+
+            parent && parent.classList.toggle("expandido");
+        });
+    })
+);
